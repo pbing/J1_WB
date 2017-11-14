50a6937e0488b97e75988f050c70bb0c3b8bf635
Bernd Beuster <bernd.beuster@gmail.com>
2017-11-14 20:46:13 +0100

    Fix wbm2.stall

--- rtl/wb_intercon.sv
+++ rtl/wb_intercon.sv
@@ -13,13 +13,16 @@ module wb_intercon
    if_wb.master wbs2_4,  // I/O
    if_wb.master wbs2_5); // I/O
 
+   wire  wbm2_valid;
+   wire  sel2_1, sel2_2, sel2_3, sel2_4, sel2_5;
    logic selr2_1, selr2_2, selr2_3, selr2_4, selr2_5;
-
-   wire sel2_1 = wbm2.adr[15:13] == 3'b000;   // RAM
-   wire sel2_2 = wbm2.adr[15:11] == 5'b00100; // I/O 4000H...4FFFH
-   wire sel2_3 = wbm2.adr[15:11] == 5'b00101; // I/O 5000H...5FFFH
-   wire sel2_4 = wbm2.adr[15:11] == 5'b00110; // I/O 6000H...6FFFH
-   wire sel2_5 = wbm2.adr[15:11] == 5'b00111; // I/O 7000H...7FFFH
+   
+   assign wbm2_valid = wbm2.cyc & wbm2.stb;
+   assign sel2_1     = wbm2_valid && (wbm2.adr[15:13] == 3'b000);   // RAM 0000H...3FFFH
+   assign sel2_2     = wbm2_valid && (wbm2.adr[15:11] == 5'b00100); // I/O 4000H...4FFFH
+   assign sel2_3     = wbm2_valid && (wbm2.adr[15:11] == 5'b00101); // I/O 5000H...5FFFH
+   assign sel2_4     = wbm2_valid && (wbm2.adr[15:11] == 5'b00110); // I/O 6000H...6FFFH
+   assign sel2_5     = wbm2_valid && (wbm2.adr[15:11] == 5'b00111); // I/O 7000H...7FFFH
 
    always_ff @(posedge wbm2.clk)
      if (wbm2.rst)
@@ -78,17 +81,17 @@ module wb_intercon
    assign wbs2_5.we    = wbm2.we;
    assign wbs2_5.dat_i = wbm2.dat_o;
 
+   assign wbm2.stall = (wbs2_1.stall & sel2_1) |
+                       (wbs2_2.stall & sel2_2) |
+                       (wbs2_3.stall & sel2_3) |
+                       (wbs2_4.stall & sel2_4) |
+                       (wbs2_5.stall & sel2_5);
+
    assign wbm2.ack   = (wbs2_1.ack & selr2_1) |
                        (wbs2_2.ack & selr2_2) |
                        (wbs2_3.ack & selr2_3) |
                        (wbs2_4.ack & selr2_4) |
                        (wbs2_5.ack & selr2_5);
-
-   assign wbm2.stall = (wbs2_1.stall & selr2_1) |
-                       (wbs2_2.stall & selr2_2) |
-                       (wbs2_3.stall & selr2_3) |
-                       (wbs2_4.stall & selr2_4) |
-                       (wbs2_5.stall & selr2_5);
 
    assign wbm2.dat_i = (wbs2_1.dat_o & {16{selr2_1}}) |
                        (wbs2_2.dat_o & {16{selr2_2}}) |
