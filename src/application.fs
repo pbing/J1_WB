fc5e9442265d65926a44a74d576307114c74b7a1
Bernd Beuster <bernd.beuster@gmail.com>
2017-12-10 14:23:26 +0100

    Add new example

--- src/application.fs
+++ src/application.fs
@@ -14,7 +14,7 @@ module[ application"
     begin again ;
 [THEN]
 
-1 [IF]
+0 [IF]
 \ 10th fibonacci
 \ with memory access
 \ 301 cycles
@@ -30,5 +30,83 @@ h# 4002 constant b
     begin again ;
 [THEN]
 
+0 [IF]
+h# 6000 constant sw
+h# 7000 constant led
+
+: main ( --- )
+    begin
+        sw @  led !
+    again ;
+[THEN]
+
+1 [IF]
+h# 6000 constant sw
+h# 7000 constant led
+d# 15 constant #leds
+
+: >thermometer  ( u1 -- u2 )  d# 1 swap lshift  1- ;
+
+: 1ms ( -- )   d# 25000 begin dup while 1- repeat drop ;
+: ms ( u -- )   begin dup while 1- 1ms repeat drop ;
+: wait ( -- )   d# 50 ms ;
+
+\ ----
+\ ---*
+\ --**
+\ -***
+\ ****
+: blink-left+ ( -- )
+    #leds 0do
+       wait       
+       i >thermometer
+       led !
+    loop ;
+
+
+\ ****
+\ ***-
+\ **--
+\ *---
+\ ----
+: blink-left- ( -- )
+    #leds 0do
+       wait
+       i >thermometer invert
+       led !
+    loop ;
+
+\ ----
+\ *---
+\ **--
+\ ***-
+\ ****
+: blink-right+ ( -- )
+    #leds 0do
+       wait
+       #leds 1- i -  >thermometer
+       led !
+    loop ;
+
+\ ****
+\ -***
+\ --**
+\ ---*
+\ ----
+: blink-right- ( -- )
+    #leds 0do
+       wait
+       #leds 1- i -  >thermometer invert
+       led !
+    loop ;
+
+: main ( -- )
+    begin
+        blink-left+
+        blink-left-
+        blink-right-
+        blink-right+
+    again ;
+[THEN]
 
 ]module
