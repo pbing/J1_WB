f088581bc9b2f9a5fa26cc677baa8819f572aa83
Bernd Beuster <bernd.beuster@creativechips.com>
2017-12-19 13:20:38 +0100

    Merged with master

--- rtl/j1_wb_qii.sv
+++ rtl/j1_wb_qii.sv
@@ -15,7 +15,7 @@ module j1_wb
     input wire   reset,          // reset
     if_wb.master wb);            // Wishbone bus
 
-   import types::*;
+   import j1_types::*;
 
    /* instruction fetch */
    logic [15:0]  instr;           // instruction
