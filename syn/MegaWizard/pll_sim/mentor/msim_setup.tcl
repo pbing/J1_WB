c32771c0e99f921dfacb2f2853a177a5d30a8d97
Bernd Beuster <bernd.beuster@gmail.com>
2018-08-12 14:48:05 +0200

    Bump to version 18.0

--- syn/MegaWizard/pll_sim/mentor/msim_setup.tcl
+++ syn/MegaWizard/pll_sim/mentor/msim_setup.tcl
@@ -1,5 +1,5 @@
 
-# (C) 2001-2017 Altera Corporation. All rights reserved.
+# (C) 2001-2018 Altera Corporation. All rights reserved.
 # Your use of Altera Corporation's design tools, logic functions and 
 # other software and tools, and its AMPP partner logic functions, and 
 # any output files any of the foregoing (including device programming 
@@ -94,7 +94,7 @@
 # within the Quartus project, and generate a unified
 # script which supports all the Altera IP within the design.
 # ----------------------------------------
-# ACDS 17.1 590 win32 2017.12.10.10:48:14
+# ACDS 18.0 614 win32 2018.08.12.14:31:27
 
 # ----------------------------------------
 # Initialize variables
@@ -113,7 +113,7 @@ if ![info exists QSYS_SIMDIR] {
 }
 
 if ![info exists QUARTUS_INSTALL_DIR] { 
-  set QUARTUS_INSTALL_DIR "C:/intelfpga_lite/17.1/quartus/"
+  set QUARTUS_INSTALL_DIR "C:/intelfpga_lite/18.0/quartus/"
 }
 
 if ![info exists USER_DEFINED_COMPILE_OPTIONS] { 
--- syn/MegaWizard/pll_sim/synopsys/vcsmx/vcsmx_setup.sh
+++ syn/MegaWizard/pll_sim/synopsys/vcsmx/vcsmx_setup.sh
@@ -1,5 +1,5 @@
 
-# (C) 2001-2017 Altera Corporation. All rights reserved.
+# (C) 2001-2018 Altera Corporation. All rights reserved.
 # Your use of Altera Corporation's design tools, logic functions and 
 # other software and tools, and its AMPP partner logic functions, and 
 # any output files any of the foregoing (including device programming 
@@ -12,7 +12,7 @@
 # or its authorized distributors. Please refer to the applicable 
 # agreement for further details.
 
-# ACDS 17.1 590 win32 2017.12.10.10:48:14
+# ACDS 18.0 614 win32 2018.08.12.14:31:27
 
 # ----------------------------------------
 # vcsmx - auto-generated simulation script
@@ -107,12 +107,12 @@
 # within the Quartus project, and generate a unified
 # script which supports all the Altera IP within the design.
 # ----------------------------------------
-# ACDS 17.1 590 win32 2017.12.10.10:48:14
+# ACDS 18.0 614 win32 2018.08.12.14:31:27
 # ----------------------------------------
 # initialize variables
 TOP_LEVEL_NAME="pll"
 QSYS_SIMDIR="./../../"
-QUARTUS_INSTALL_DIR="C:/intelfpga_lite/17.1/quartus/"
+QUARTUS_INSTALL_DIR="C:/intelfpga_lite/18.0/quartus/"
 SKIP_FILE_COPY=0
 SKIP_DEV_COM=0
 SKIP_COM=0
